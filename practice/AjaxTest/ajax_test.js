3d1527960bfdeedd3cf34df2730a1d34a2b03348
chogori18 <chogori18@gmail.com>
2022-02-18 16:23:11 +0900

    ajax test.jsに追加

--- practice/AjaxTest/ajax_test.js
+++ practice/AjaxTest/ajax_test.js
@@ -1,5 +1,20 @@
 var btnLoad = document.querySelector('#load');
 //読み込みボタンのクリックイベントハンドラを定義
 btnLoad.addEventListener('click', function (event) {
-  //ここにJSONを読み込む処理を記述する
+  //【手順１】XMLHttpRequestオブジェクトのインスタンスを生成
+  var xmlHttpRequest = new XMLHttpRequest();
+  //【手順2】通信状態の変化を監視するイベントハンドラを設定
+  xmlHttpRequest.onreadystatechange = function() {
+    // レスポンスの受信が正常に完了したとき
+    if (this.readyState == 4 && this.status == 200) {
+      //受信下データをコンソールに出力する
+      console.log(this.readyState, this.response);
+    }
+};
+//「手順３」レスポンスの形式を指定する
+xmlHttpRequest.responseType = 'json';
+//【手順４】リクエストメソッドと読み込みファイルのパスを指定する
+xmlHttpRequest.open('GET', 'product.json');
+//【手順５】リクエストを送信する（非同期通信を開始する）
+xmlHttpRequest.send();
 });
