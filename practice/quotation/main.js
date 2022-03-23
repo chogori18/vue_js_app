1d89715772c7ee6d998323f2b4f37a3031fe03c7
chogori18 <chogori18@gmail.com>
2022-03-23 13:20:58 +0900

    ページ読み込みイベントハンドラ追加

--- practice/quotation/main.js
+++ practice/quotation/main.js
@@ -24,9 +24,19 @@ app.querySelector('#opt4').addEventListener('input', onInputChanged, false);
 
 // ページの読み込みが完了したとき呼び出されるイベントハンドラ
 function onPageLoad(event) {
+    // フォームコントロールを取得
+    var wedding_date = app.querySelector('#wedding_date'); // 挙式日
+    var delivery_date = app.querySelector('#delivery_date'); // DVD仕上がり予定日
+    // 今日の日付を取得
+    var dt = new Date();
     // 挙式日に2か月後の日付を設定
+    dt.setMonth(dt.getMonth() + 2);
+    wedding_date.value = formatDate(dt);
     // DVD仕上がり予定日に、挙式日の1週間前の日付を設定
+    dt.setDate(dt.getDate() - 7);
+    delivery_date.value = formatDate(dt);
     // DVD仕上がり予定日に翌日以降しか入力できないようにする
+    delivery_date.setAttribute('min', tommorow());
     // フォームの表示を更新する
     updateForm();
 }
@@ -41,7 +51,22 @@ function onInputChanged(event) {
  * 関数
  */
 
-//金額の表示を更新する関数
+// 日付をYYYY-MM-DDの書式で返すメソッド
+function formatDate(dt) {
+    var y = dt.getFullYear();
+    var m = ('00' + (dt.getMonth() + 1)).slice(-2);
+    var d = ('00' + dt.getDate()).slice(-2);
+    return (y + '-' + m + '-' +d);
+}
+
+// 明日の日付をYYYY-MM-DDの書式で返す関数
+function tommorow() {
+    var dt = new Date();
+    dt.setDate(dt.getDate() + 1);
+    return formatDate(dt);
+}
+
+// 金額の表示を更新する関数
 function updateForm() {
     // 金額を再計算
     // 表示を更新
