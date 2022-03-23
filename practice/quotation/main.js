8c13282e454b118dd85171562690d0a26ba4f7b2
chogori18 <chogori18@gmail.com>
2022-03-23 11:44:35 +0900

    js作成

---
+++ practice/quotation/main.js
@@ -0,0 +1,48 @@
+// コンポーネントのルートノード
+var app = document.querySelector('#app');
+// 消費税率
+var taxRate = 0.1;
+
+/**
+ * イベントハンドラの割り当て
+ */
+
+// ページの読み込み完了イベント
+window.addEventListener('load', onPageLoad, false);
+// 入力内容変更イベント（DVD仕上がり予定日）
+app.querySelector('#delivery_date').addEventListener('change', onInputChanged, false);
+// 入力内容変更イベント（BGM手配）
+app.querySelector('#opt1').addEventListener('change', onInputChanged, false);
+// 入力内容変更イベント（撮影）
+app.querySelector('#opt2').addEventListener('change', onInputChanged, false);
+// 入力内容変更イベント（DVD盤面印刷）
+app.querySelector('#opt4').addEventListener('input', onInputChanged, false);
+
+/**
+ * イベントハンドラ
+ */
+
+// ページの読み込みが完了したとき呼び出されるイベントハンドラ
+function onPageLoad(event) {
+    // 挙式日に2か月後の日付を設定
+    // DVD仕上がり予定日に、挙式日の1週間前の日付を設定
+    // DVD仕上がり予定日に翌日以降しか入力できないようにする
+    // フォームの表示を更新する
+    updateForm();
+}
+
+// 入力内容を変更したとき呼び出されるイベントハンドラ
+function onInputChanged(event) {
+    // フォームの表示を更新する
+    updateForm();
+}
+
+/**
+ * 関数
+ */
+
+//金額の表示を更新する関数
+function updateForm() {
+    // 金額を再計算
+    // 表示を更新
+}
