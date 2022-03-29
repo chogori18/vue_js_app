f6fdbafbcb76bb99d41b63500897722808ef5549
chogori18 <chogori18@gmail.com>
2022-03-29 15:48:13 +0900

    基本料金とオプション料金

--- practice/quotation/main.js
+++ practice/quotation/main.js
@@ -66,11 +66,57 @@ var app = new Vue({
         },
         // 基本料金（税込）を返す算出プロパティ
         taxedBasePrice: function() {
-            // TODO：基本料金（税込）を計算して返す
+            // 割増料金
+            var addPrice = 0;
+            // 納期までの残り日数を計算
+            var dateDiff = this.getDateDiff(this.delivery_date,(new Date()).toLocaleString());
+            // 割増料金を求める
+            if (21 <= dateDiff && dateDiff < 30) {
+                // 納期が1か月未満の場合
+                addPrice = this.addPrice1;
+            }
+            else if (14 <= dateDiff && dateDiff < 21) {
+                // 納期が3週間未満の場合
+                addPrice = this.addPrice2;
+            }
+            else if (7 <= dateDiff && dateDiff < 14) {
+                // 納期が2週間未満の場合
+                addPrice = this.addPrice3;
+            }
+            else if (3 < dateDiff && dateDiff < 7) {
+                // 納期が1週間未満の場合
+                addPrice = this.addPrice4;
+            }
+            else if (dateDiff == 3) {
+                // 納期が3日後の場合
+                addPrice = this.addPrice5;
+            }
+            else if (dateDiff == 2) {
+                // 納期が2日後の場合
+                addPrice = this.addPrice6;
+            }
+            else if (dateDiff == 1) {
+                // 納期が翌日の場合
+                addPrice = this.addPrice7;
+            }
+            // 基本料金（税込）を返す
+            return this.incTax(this.basePrice + addPrice);
         },
         // オプション料金（税込）を返す算出プロパティ
         taxedOptPrice: function() {
-            // TODO：オプション料金（税込）を計算して返す
+            // オプション料金
+            var optPrice = 0;
+            // BGM手配
+            if (this.opt1_use) { optPrice += this.opt1_price; }
+            // 撮影
+            if (this.opt2_use) { optPrice += this.opt2_price; }
+            // DVD盤面印刷
+            if (this.opt3_use) { optPrice += this.opt3_price; }
+            // 写真スキャニング
+            if (this.opt4_num == '') { this.opt4_num = 0; }
+            optPrice += this.opt4_num * this.opt4_price;
+            // オプション料金（税込）を返す
+            return this.incTax(optPrice);
         },
         // 合計金額（税込）を返す算出プロパティ
         taxedTotalPrice: function() {
